ceca35b68d69167a8fce05d200f1d09fd31b1626
Renato Britto <renatobritto@protonmail.com>
2026-02-02 16:10:24 -0300

    Add scripts for browser and git

---
+++ installs.sh
@@ -0,0 +1,36 @@
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install --reinstall gnome-control-center
+sudo apt install -y\
+    git\
+    terminator\
+    gh\
+    curl\
+    build-essential\
+    libgtk-3-dev\
+    plocate\
+    htop\
+    python3-pip\
+    python3-dev\
+    python3-venv\
+    curl\
+    wget\
+    git\
+    vim\
+    build-essential\
+    software-properties-common\
+    gnupg\
+    scdaemon\
+    pcscd\
+    python3\
+    python-is-python3\
+    python3-pip\
+    python3.10-venv\
+    calibre\
+    vlc\
+    gcc\
+    make
+
+sudo snap install --classic \
+    obsidian 
